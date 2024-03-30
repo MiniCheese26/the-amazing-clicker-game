2feb906661e73d0440c0381a240c88b4a665295a
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-03-30 23:50:23 +0000

    add qr code on leaderboard

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -5,6 +5,7 @@
 	import type { LeaderboardRow } from '$lib/types';
 	import TablerArrowUp from '~icons/tabler/arrow-up';
 	import TablerArrowDown from '~icons/tabler/arrow-down';
+	import { renderSVG } from 'uqr';
 
 	type LeaderboardRowWithChanges = {
 		change?: 'up' | 'down';
@@ -18,6 +19,8 @@
 	let now = dayjs();
 	let nextUpdate = now.add(5, 's');
 	let updating = false;
+	let svg: string;
+	let showQr = false;
 
 	const onPoll = async () => {
 		updating = true;
@@ -58,6 +61,7 @@
 	};
 
 	onMount(() => {
+		svg = renderSVG(location.origin);
 		nowInterval = setInterval(onNow, 1000);
 		pollingInterval = setInterval(onPoll, 5000);
 	});
@@ -72,46 +76,55 @@
 	<h1 class="text-4xl md:text-4xl font-medium text-center">Leaderboard</h1>
 	<a href="/" class="underline cursor-pointer text-3xl">Play the Game!</a>
 </nav>
-<main class="flex flex-col items-center justify-center flex-1 my-4 gap-4">
-	<h2 class="text-2xl">
-		{updating ? 'Updating...' : `Updating in ${nextUpdate.diff(now, 's')} seconds`}
-	</h2>
-	<table class="border-separate border-spacing-2 border border-slate-500">
-		<thead class="text-3xl">
-			<tr>
-				<th class="min-w-8"></th>
-				<th>Place</th>
-				<th>Name</th>
-				<th>Clicks</th>
-			</tr>
-		</thead>
-		<tbody class="text-2xl">
-			{#if leaderboard.length > 0}
-				{#each leaderboard as row, index}
-					<tr>
-						{#if row.change === 'down'}
-							<td class="text-red-500"><TablerArrowDown /></td>
-						{:else if row.change === 'up'}
-							<td class="text-green-500"><TablerArrowUp /></td>
-						{:else}
-							<td></td>
-						{/if}
-						<td>#{index + 1}</td>
-						{#if row.change === 'down'}
-							<td class="text-red-500">{row.name}</td>
-						{:else if row.change === 'up'}
-							<td class="text-green-500">{row.name}</td>
-						{:else}
-							<td>{row.name}</td>
-						{/if}
-						<td>{row.clicks}</td>
-					</tr>
-				{/each}
-			{:else}
+<main class="flex flex-col items-center flex-1 my-4 relative">
+	{#if svg && showQr}
+		<button class="*:h-96 absolute" on:click={() => (showQr = false)}>
+			{@html svg}
+		</button>
+	{:else}
+		<button on:click={() => (showQr = true)} class="bg-neutral-900 p-4 md:p-2 rounded-lg md:rounded-md border-2 border-neutral-100 text-neutral-100">Show QR Code</button>
+	{/if}
+	<div class="flex-1 flex flex-col justify-center items-center gap-4">
+		<h2 class="text-2xl">
+			{updating ? 'Updating...' : `Updating in ${nextUpdate.diff(now, 's')} seconds`}
+		</h2>
+		<table class="border-separate border-spacing-2 border border-slate-500">
+			<thead class="text-3xl">
 				<tr>
-					<td colspan="4" class="text-center">No one's here :(</td>
+					<th class="min-w-8"></th>
+					<th>Place</th>
+					<th>Name</th>
+					<th>Clicks</th>
 				</tr>
-			{/if}
-		</tbody>
-	</table>
+			</thead>
+			<tbody class="text-2xl">
+				{#if leaderboard.length > 0}
+					{#each leaderboard as row, index}
+						<tr>
+							{#if row.change === 'down'}
+								<td class="text-red-500"><TablerArrowDown /></td>
+							{:else if row.change === 'up'}
+								<td class="text-green-500"><TablerArrowUp /></td>
+							{:else}
+								<td></td>
+							{/if}
+							<td>#{index + 1}</td>
+							{#if row.change === 'down'}
+								<td class="text-red-500">{row.name}</td>
+							{:else if row.change === 'up'}
+								<td class="text-green-500">{row.name}</td>
+							{:else}
+								<td>{row.name}</td>
+							{/if}
+							<td>{row.clicks}</td>
+						</tr>
+					{/each}
+				{:else}
+					<tr>
+						<td colspan="4" class="text-center">No one's here :(</td>
+					</tr>
+				{/if}
+			</tbody>
+		</table>
+	</div>
 </main>
