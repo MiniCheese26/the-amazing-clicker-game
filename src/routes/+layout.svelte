66bc45602003be0f43aa97ee5caf65a8ad427e4a
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-03-30 20:03:41 +0000

    switch to pnpm

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,11 +9,16 @@
 			Made by <a
 				href="https://emilymedhurst.gay"
 				target="_blank"
-				rel="noreferrer noopener"
+				rel="noreferrer noopener me"
 				class="underline cursor-pointer">Emily Medhurst</a
 			>
 			/
-			<a href="" target="_blank" rel="noreferrer noopener" class="underline cursor-pointer">Code</a>
+			<a
+				href="https://github.com/MiniCheese26/the-amazing-clicker-game"
+				target="_blank"
+				rel="noreferrer noopener"
+				class="underline cursor-pointer">Code</a
+			>
 		</p>
 	</footer>
 </div>
